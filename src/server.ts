8007cfa1c6825ef118d7540921ccc031b09e452a
Dsblima <dsbl@ecomp.poli.br>
2022-03-16 22:10:03 -0300

    Add uuid

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/cateogries.routes";
+
 const app = express();
 
-app.get("/", (resquest, response) => {
-    return response.json({ message: "Hello World!!!!" });
-});
+app.use(express.json());
 
-app.post("/courses", (request, response) => {
-    const {name} = request.body;
-    return response.json({name});
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!!"));
