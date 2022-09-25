706b1ce4ef7761fec29ce7da1e5eb1411a4d8993
Dsblima <dsbl@ecomp.poli.br>
2022-09-25 13:15:28 -0300

    Create User Repository

---
+++ src/modules/accounts/entities/repositories/implementations/UsersRepositort.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../models/User";
+import { IUserRepository } from "../interfaces/IUsersRepository";
+
+class UsersRepository implements IUserRepository {
+    private repository: Repository<User>;
+
+    constructor() {
+        this.repository = getRepository(User);
+    }
+
+    async create({
+        name,
+        username,
+        password,
+        email,
+        driver_license,
+    }: ICreateUserDTO): Promise<void> {
+        const user = this.repository.create({
+            name,
+            username,
+            password,
+            email,
+            driver_license,
+        });
+
+        await this.repository.save(user);
+    }
+}
+
+export { UsersRepository };
