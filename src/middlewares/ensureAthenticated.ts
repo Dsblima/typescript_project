9282695ae33bcf781a15be150c79537ffbe12916
Dsblima <dsbl@ecomp.poli.br>
2022-10-08 17:33:29 -0300

    Authenticate routes

---
+++ src/middlewares/ensureAthenticated.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+    sub: string;
+}
+
+export async function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new Error("Token missing!");
+    }
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const { sub: user_id } = verify(
+            token,
+            "08b9d906f1f2534056d561f74bdd6dd9"
+        ) as IPayload;
+
+        const usersRepository = new UsersRepository();
+        const user = usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error("User does not exists!");
+        }
+        next();
+    } catch {
+        throw new Error("Invalid token");
+    }
+}
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 
@@ -8,8 +9,8 @@ const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
-
 specificationsRoutes.get("/", listSpecificationsController.handle);
 
 export { specificationsRoutes };
