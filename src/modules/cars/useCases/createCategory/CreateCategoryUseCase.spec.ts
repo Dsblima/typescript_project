8fdcdc25a6a682c7c0a8c20eb9f5c2d6960fbe96
Dsblima <dsbl@ecomp.poli.br>
2022-12-20 10:19:24 -0300

    Add Jest library

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create Category", () => {
+    it("Should be able to...", () => {
+        const soma = 2 + 2;
+        const resultado = 4;
+
+        expect(soma).toBe(resultado);
+    });
+
+    it("Should not be...", () => {
+        const soma = 2 + 2;
+        const resultado = 5;
+
+        expect(soma).not.toBe(resultado);
+    });
+});
