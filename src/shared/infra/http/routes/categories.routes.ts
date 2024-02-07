e77893991abcdf3691027bfe864e6a168b9de92c
Dsblima <dsbl@ecomp.poli.br>
2024-02-07 01:26:34 -0300

    Fix eslint errors

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,30 +1,38 @@
-import { Router } from "express";
-import multer from "multer";
+import { Router } from 'express';
+import multer from 'multer';
 
-import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
-import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
-import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
-import { ensureAuthenticated } from "../middlewares/ensureAthenticated";
-import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
+import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
+import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAthenticated';
 
 const categoriesRoutes = Router();
 
 const upload = multer({
-    dest: "./tmp",
+    dest: './tmp',
 });
 
 const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", ensureAuthenticated, ensureAdmin, createCategoryController.handle);
+categoriesRoutes.post(
+    '/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createCategoryController.handle
+);
 
-categoriesRoutes.get("/", listCategoriesController.handle);
+categoriesRoutes.get('/', listCategoriesController.handle);
 
 categoriesRoutes.post(
-    "/import",
-    upload.single("File"),
-    ensureAuthenticated, ensureAdmin, importCategoryController.handle
+    '/import',
+    upload.single('File'),
+    ensureAuthenticated,
+    ensureAdmin,
+    importCategoryController.handle
 );
 
 export { categoriesRoutes };
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
-import { ListSpecificationsController } from "@modules/cars/useCases/listSpecifications/ListSpecificationsController";
+import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { ListSpecificationsController } from '@modules/cars/useCases/listSpecifications/ListSpecificationsController';
 
-import { ensureAuthenticated } from "../middlewares/ensureAthenticated";
-import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAthenticated';
 
 const specificationsRoutes = Router();
 
@@ -12,7 +12,12 @@ const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
 specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
-specificationsRoutes.get("/", listSpecificationsController.handle);
+specificationsRoutes.post(
+    '/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createSpecificationController.handle
+);
+specificationsRoutes.get('/', listSpecificationsController.handle);
 
 export { specificationsRoutes };
