dbc383fa716a5bf06e5c317485ea1a4c358ec9c0
Dsblima <dsbl@ecomp.poli.br>
2024-06-26 06:26:17 -0300

    Add migration running in server initialization

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,5 +4,6 @@ import { app } from './app';
 
 app.listen(3333, async () => {
     await connectionSource.initialize();
+    await connectionSource.runMigrations();
     console.log('Server is running!!');
 });
